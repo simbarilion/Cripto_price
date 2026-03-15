adb244d58d02897c490a2d5783becef77da19c00
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-03-16 05:34:35 +0700

    modified tests and README.md

--- app/tests/test_services.py
+++ app/tests/test_services.py
@@ -2,22 +2,7 @@ from unittest.mock import patch
 
 import pytest
 
-from app.db.database import SessionLocal
-from app.db.models import Price
 from app.services.price_fetcher import fetch_and_store_prices
-from app.services.price_service import save_price
-
-
-def test_save_price():
-    """Проверяет сохранение цен в базу данных"""
-    ticker = "btc_usd"
-    price = 50000
-    save_price(ticker, price)
-    db = SessionLocal()
-    result = db.query(Price).filter(Price.ticker == ticker).order_by(Price.timestamp.desc()).first()
-    db.close()
-    assert result is not None
-    assert result.price == price
 
 
 @pytest.mark.asyncio
